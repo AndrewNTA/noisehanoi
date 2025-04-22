0c13998bfa530c1309685b12ae182e64e8c1605a
andrew nguyen <andrew.nta87@gmail.com>
2025-04-22 23:24:10 +0700

    [nextjs] remove unused

--- app/components/SpotifyIframe.js
+++ app/components/SpotifyIframe.js
@@ -10,7 +10,7 @@ export default function SpotifyIframe() {
         width="100%"
         height="152"
         frameBorder="0"
-        allowfullscreen=""
+        allowFullScreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       ></iframe>
