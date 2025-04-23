a590cb564538e52e7401e3943c41d0160bcc8bc3
andrew nguyen <andrew.nta87@gmail.com>
2025-04-23 11:58:12 +0700

    [nextjs] fix warning

--- app/artists/Group.js
+++ app/artists/Group.js
@@ -2,32 +2,31 @@
 
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
-import useStyles from './styles';
+import { styled } from '@mui/material/styles'
 
-import ImgGr1T1 from '../static/images/avatar_gr1_1.jpg';
-import ImgGr1T2 from '../static/images/avatar_gr1_2.jpg';
-import ImgGr1T3 from '../static/images/avatar_gr1_3.jpeg';
+import ImgGr1T1 from '../static/images/avatar_gr1_1.jpg'
+import ImgGr1T2 from '../static/images/avatar_gr1_2.jpg'
+import ImgGr1T3 from '../static/images/avatar_gr1_3.jpeg'
 
-import ImgGr2T1 from '../static/images/avatar_gr2_1.jpg';
-import ImgGr2T2 from '../static/images/avatar_gr2_2.jpg';
-import ImgGr2T3 from '../static/images/avatar_gr2_3.jpg';
+import ImgGr2T1 from '../static/images/avatar_gr2_1.jpg'
+import ImgGr2T2 from '../static/images/avatar_gr2_2.jpg'
+import ImgGr2T3 from '../static/images/avatar_gr2_3.jpg'
 
-import ImgGr3T1 from '../static/images/avatar_gr3_1.jpg';
-import ImgGr3T2 from '../static/images/avatar_gr3_2.jpg';
-import ImgGr3T3 from '../static/images/avatar_gr3_3.jpg';
+import ImgGr3T1 from '../static/images/avatar_gr3_1.jpg'
+import ImgGr3T2 from '../static/images/avatar_gr3_2.jpg'
+import ImgGr3T3 from '../static/images/avatar_gr3_3.jpg'
 
-import ImgGr4T1 from '../static/images/avatar_gr4_1.jpeg';
-import ImgGr4T2 from '../static/images/avatar_gr4_2.jpeg';
-import ImgGr4T3 from '../static/images/avatar_gr4_3.jpg';
+import ImgGr4T1 from '../static/images/avatar_gr4_1.jpeg'
+import ImgGr4T2 from '../static/images/avatar_gr4_2.jpeg'
+import ImgGr4T3 from '../static/images/avatar_gr4_3.jpg'
 
-import ImgGr5T1 from '../static/images/avatar_gr5_1.jpg';
-import ImgGr5T2 from '../static/images/avatar_gr5_2.jpg';
-import ImgGr5T3 from '../static/images/avatar_gr5_3.jpg';
+import ImgGr5T1 from '../static/images/avatar_gr5_1.jpg'
+import ImgGr5T2 from '../static/images/avatar_gr5_2.jpg'
+import ImgGr5T3 from '../static/images/avatar_gr5_3.jpg'
 
-import ImgGr6T1 from '../static/images/avatar_gr6_1.jpg';
-import ImgGr6T2 from '../static/images/avatar_gr6_2.jpg';
-import ImgGr6T3 from '../static/images/avatar_gr6_3.jpg';
-
+import ImgGr6T1 from '../static/images/avatar_gr6_1.jpg'
+import ImgGr6T2 from '../static/images/avatar_gr6_2.jpg'
+import ImgGr6T3 from '../static/images/avatar_gr6_3.jpg'
 
 const groupList = [
   {
@@ -40,7 +39,6 @@ const groupList = [
     title: 'Reggae / Hip hop / Afrobeat',
     images: [ImgGr2T1, ImgGr2T2, ImgGr2T3],
   },
-
   {
     id: 'punk_metal_grunge',
     title: 'Punk / Metal / Grunge',
@@ -51,7 +49,6 @@ const groupList = [
     title: 'Soul / Jazz / Blues / Folk',
     images: [ImgGr4T1, ImgGr4T2, ImgGr4T3],
   },
-
   {
     id: 'electronic_experimental_other',
     title: 'Electronic / Experimental / Other',
@@ -62,36 +59,87 @@ const groupList = [
     title: 'Cover bands and tributes',
     images: [ImgGr6T1, ImgGr6T2, ImgGr6T3],
   },
-];
+]
+
+const GroupContainer = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap',
+})
+
+const GroupBox = styled('div')({
+  width: '49%',
+  boxSizing: 'border-box',
+  padding: '16px',
+  marginBottom: '2rem',
+  border: '1px solid #ffffff',
+  borderRadius: '4px',
+  cursor: 'pointer',
+  '&:hover': {
+    transform: 'translateY(-4px)',
+    transition: 'transform 0.2s',
+  },
+  '@media (max-width: 600px)': {
+    width: '100%',
+    padding: '10px',
+    marginBottom: '1rem',
+  },
+})
+
+const GroupTitle = styled('p')({
+  color: '#ffffff',
+  fontSize: '1rem',
+  fontWeight: 700,
+  fontStyle: 'italic',
+  marginTop: 0,
+  marginBottom: '1rem',
+  '@media (max-width: 600px)': {
+    fontWeight: 600,
+  },
+})
+
+const GroupImagesContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+const GroupImage = styled(Image)({
+  float: 'left',
+  width: '30%',
+  height: '6rem',
+  objectFit: 'cover',
+  '@media (max-width: 600px)': {
+    height: '3.5rem',
+  },
+})
 
 export default function Group() {
-  const router = useRouter();
-  const classes = useStyles();
+  const router = useRouter()
 
   const goToGroupDetail = (id) => {
-    router.push(`/artists/?group_key=${id}&page_id=1`);
+    router.push(`/artists/?group_key=${id}&page_id=1`)
   }
 
-  const renderImg = (src) => (
-    <Image src={src} alt="not found" className={classes.groupBandImg} />
-  );
+  const renderImg = (src, index) => (
+    <GroupImage
+      key={`${src}-${index}`}
+      src={src}
+      alt="group"
+      width={200}
+      height={200}
+    />
+  )
 
   const renderItem = (item) => (
-    <div
-      key={item.id}
-      className={classes.groupBandBox}
-      onClick={() => goToGroupDetail(item.id)}
-    >
-      <p className={classes.groupBandTitle}>{item.title}</p>
-      <div className={classes.groupBandImagesContainer}>
-        {item.images.map((src) => renderImg(src))}
-      </div>
-    </div>
-  );
+    <GroupBox key={item.id} onClick={() => goToGroupDetail(item.id)}>
+      <GroupTitle>{item.title}</GroupTitle>
+      <GroupImagesContainer>
+        {item.images.map((src, index) => renderImg(src, index))}
+      </GroupImagesContainer>
+    </GroupBox>
+  )
 
-  return (
-    <div className={classes.groupBandContainer}>
-      {groupList.map((item) => renderItem(item))}
-    </div>
-  );
+  return <GroupContainer>{groupList.map(renderItem)}</GroupContainer>
 } 
